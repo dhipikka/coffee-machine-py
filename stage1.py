5cb484f16d11c3a5e920edf288113a97dd13d9ac
Dia <34603038+diavarala@users.noreply.github.com>
2020-05-05 19:52:13 +0530

    added quantities

---
+++ stage1.py
@@ -0,0 +1,10 @@
+# Write your code here
+print('Write how many cups of coffee you will need:')
+n = int(input())
+print('For ' + str(n) + ' cups of coffee you will need:')
+water = n * 200
+milk = n * 50
+beans = n * 15
+print(str(water) + ' ml of water')
+print(str(milk) + ' ml of milk')
+print(str(beans) + ' ml of coffee beans')
