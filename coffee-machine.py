dce501bcc0eb1c57477f57ecb772e38de857ffbf
Dia <34603038+diavarala@users.noreply.github.com>
2020-05-05 20:05:31 +0530

    add amounts

--- coffee-machine.py
+++ coffee-machine.py
@@ -1,3 +1,12 @@
+print('Write how many cups of coffee you will need:')
+n = int(input())
+print('For ' + str(n) + ' cups of coffee you will need:')
+water = n * 200
+milk = n * 50
+beans = n * 15
+print(str(water) + ' ml of water')
+print(str(milk) + ' ml of milk')
+print(str(beans) + ' ml of coffee beans')
 print("Starting to make a coffee")
 print("Grinding coffee beans")
 print("Boiling water")
